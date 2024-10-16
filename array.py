9584a8e37061082a4b8fc2a35b8e24b9336afb16
jalexanderf <47376673+jfernandezp@users.noreply.github.com>
2024-10-16 17:50:30 -0500

    Create array.py

---
+++ array.py
@@ -0,0 +1,12 @@
+array = ["Peru", 'Mexico', 'Francia', "Alemania", "Brazil", "Colombia"]
+index = array.index('Mexico')
+array.append("Argentina")
+array.insert(2, "Bolivia")
+array.pop(1)
+array.remove('Brazil')
+array.extend('Uruguay')
+array.reverse()
+array.sort()
+print(index)
+for i in array:
+    print(i)
